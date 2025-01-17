2ff57219f03dee39bb511827728e11b1be97daa7
deepak <dlipkumar77@gmail.com>
2025-01-17 17:44:05 +0530

    Full screen bottom model sheet

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -39,6 +39,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpenses(
         onAddExpense: _addExpeses,
